2886d1ee11e79561278a5f5e99e2ecaf1a2f1817
sunsobolev <36673718+sunsobolev@users.noreply.github.com>
2023-08-21 13:25:02 +0300

    error

---
+++ SimpleApp/project/simpleapp/views.py
@@ -0,0 +1,8 @@
+from django.views.generic import ListView
+from .models import Product
+
+class ProductsList(ListView):
+    model = Product
+    ordering = 'name'
+    template_name = 'products.html'
+    context_object_name = 'products'
